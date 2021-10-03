81cce5d4b41ac90bb3e65e8822b30fa27427602c
Igor <polygor@gmail.com>
2021-10-03 23:08:15 +0300

    202110032308

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ let menu = document.querySelector('.js-menu');
 //   const foodList = array.map(food => menuTemplateElement(food)).join('');
 //   menu.insertAdjacentHTML('beforeend', foodList);
 // };
-
 // createFoodMenu(menuDataElements);
 
 const foodMenu = menuDataElements.reduce((acc, item) =>
   acc + menuTemplateElement(item), "");
-
 menu.insertAdjacentHTML("afterbegin", foodMenu);
